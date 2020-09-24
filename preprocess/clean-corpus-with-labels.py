6964a6e3bd3925574b336afaf7b2deb2099f81d9
Hiroyuki Deguchi <deguchi@ai.cs.ehime-u.ac.jp>
2020-09-24 15:22:45 +0900

    bug fix: preprocess/clean-corpus-with-labels.py

--- preprocess/clean-corpus-with-labels.py
+++ preprocess/clean-corpus-with-labels.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 from argparse import ArgumentParser
+import itertools
 import sys
 
 
@@ -18,12 +19,15 @@ def main(args):
     lines = 0
     label_in = [open('{}.{}'.format(args.corpus, l), 'r') for l in args.label_ext]
     label_out = [open('{}.{}'.format(args.clean_corpus, l), 'w') for l in args.label_ext]
+    if len(label_in) < 1:
+        label_in.append(itertools.cycle([None]))
+        label_out.append(itertools.cycle([None]))
     with \
       open(f, 'r', encoding='utf-8') as fp, \
       open(e, 'r', encoding='utf-8') as ep, \
       open(fo, 'w', encoding='utf-8') as fop, \
       open(eo, 'w', encoding='utf-8') as eop:
-        for fl, el, ll in zip(fp, ep, *label_in):
+        for fl, el, *lip in zip(fp, ep, *label_in):
             lines += 1
             fn = len(fl.strip().split())
             en = len(el.strip().split())
@@ -37,20 +41,22 @@ def main(args):
                 continue
             fop.write(fl)
             eop.write(el)
-            for lop in label_out:
-                lop.write(ll)
+            if len(args.label_ext) > 0:
+                for ll, lop in zip(lip, label_out):
+                    lop.write(ll)
             cnt += 1
 
-    for lip, lop in zip(label_in, label_out):
-        lip.close()
-        lop.close()
+    if len(args.label_ext) > 0:
+        for lip, lop in zip(label_in, label_out):
+            lip.close()
+            lop.close()
 
     print('input sentences: {}, output sentences: {}'.format(lines, cnt), file=sys.stderr)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--label-ext', '-l', action='append')
+    parser.add_argument('--label-ext', '-l', action='append', default=[])
     parser.add_argument('--ratio', type=float, default=9)
     parser.add_argument('corpus')
     parser.add_argument('l1')
