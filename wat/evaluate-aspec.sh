92dc7a979b9f84a7f8c5535331eea2739e733525
Hiroyuki Deguchi <deguchi@ai.cs.ehime-u.ac.jp>
2021-02-05 13:52:20 +0900

    add: wat/evaluate-aspec.sh evaluation for ASPEC-JC

--- wat/evaluate-aspec.sh
+++ wat/evaluate-aspec.sh
@@ -15,10 +15,10 @@ function usage() {
     fi
 
     cat << __EOT__ >&2
-usage: $(basename $0) [ ja | en ]
+usage: $(basename $0) [ ja | en | zh ]
 
 arguments:
-    [ ja | en ]     language code
+    [ ja | en | zh ]     language code
 
 environment variables:
     TEST_DATA       set the /path/to/ASPEC/ASPEC-JE/test/test.txt (required)
@@ -30,10 +30,15 @@ __EOT__
     exit -1
 }
 
+set -eu
+TEST_DATA=$(readlink -f $TEST_DATA)
+ref_filename=$(basename $TEST_DATA)
+test_set=$(basename $TEST_DATA .txt)
+CORPORA=$(readlink -f $(dirname $TEST_DATA)/..)
+
 OUTDIR=references
-prep=$OUTDIR
+prep=$OUTDIR/$CORPORA
 orig=$prep/orig
-ref=$(basename $TEST_DATA .txt)
 
 if ! [[ -d $DNLP_CACHE_DIR ]]; then
     mkdir -p $DNLP_CACHE_DIR
@@ -48,38 +53,44 @@ mkdir -p $WORKDIR
 cd $WORKDIR
 
 if [[ -z $TEST_DATA ]] || ! [[ -f $TEST_DATA ]]; then
-    if ! [[ -f $prep/$(basename $TEST_DATA) ]]; then
+    if ! [[ -f $prep/$ref_filename ]]; then
         echo "reference file not found." >&2
         usage "TEST_DATA '$TEST_DATA' not found."
     fi
 fi
 
 if [[ $# -ne 1 ]] || \
-       ([[ $1 != ja ]] && [[ $1 != en ]]); then
+       ([[ $1 != ja ]] && [[ $1 != en ]] && [[ $1 != zh ]]); then
     usage
 fi
 tgt=$1
 
 MOSES_SCRIPTS=mosesdecoder-2.1.1/scripts
 MOSES_TOKENIZER=$MOSES_SCRIPTS/tokenizer/tokenizer.perl
-MOSES_BLEU=$MOSES_SCRIPTS/generic/multi-bleu.perl
 KYTEA_ROOT=kytea-0.4.6
 KYTEA_TOKENIZER=$KYTEA_ROOT/bin/kytea
-KYTEA_MODELNAME=jp-0.4.2-utf8-1.mod
-KYTEA_MODEL=$KYTEA_ROOT/$KYTEA_MODELNAME
+KYTEA_MODELNAME_JA=jp-0.4.2-utf8-1.mod
+KYTEA_MODELNAME_ZH=msr-0.4.0-1.mod
+KYTEA_MODEL_JA=$KYTEA_ROOT/$KYTEA_MODELNAME_JA
+KYTEA_MODEL_ZH=$KYTEA_ROOT/$KYTEA_MODELNAME_ZH
 CC=/usr/bin/gcc
 WAT_SCRIPTS=WAT-scripts
 SEG_SCRIPTS=script.segmentation.distribution
 
+# for evaluation
+MOSES_BLEU=$MOSES_SCRIPTS/generic/multi-bleu.perl
+RIBES_DIR=RIBES-1.02.4
+RIBES_SCRIPT=$RIBES_DIR/RIBES.py
+
 if ! [[ -d $MOSES_SCRIPTS ]]; then
-    echo 'Cloning Moses github repository (for tokenization scripts)...'
+    echo 'Cloning Moses github repository (for tokenization and evaluation)...'
     git clone https://github.com/moses-smt/mosesdecoder.git \
         -b RELEASE-2.1.1 \
         mosesdecoder-2.1.1
 fi >&2
 
-if ! [[ -d $KYTEA_ROOT ]]; then
-    echo 'Downloading Kytea source code (for tokenization scripts)...'
+if ([[ $tgt = "ja"]] || [[ $tgt = "zh"]]) && ! [[ -d $KYTEA_ROOT ]]; then
+    echo 'Downloading Kytea source code (for tokenization)...'
     curl http://www.phontron.com/kytea/download/kytea-0.4.6.tar.gz | tar xzf -
 fi >&2
 
@@ -94,7 +105,12 @@ if ! [[ -d $SEG_SCRIPTS ]]; then
     sed -ie "s/use encoding 'utf8';/use utf8;/g" $SEG_SCRIPTS/z2h-utf8.pl
 fi >&2
 
-if ! [[ -f $KYTEA_TOKENIZER ]]; then
+if ! [[ -f $RIBES_SCRIPT ]]; then
+    echo 'Downloading RIBES script (for evaluation)...'
+    curl http://www.kecl.ntt.co.jp/icl/lirg/ribes/package/RIBES-1.02.4.tar.gz | tar xzf -
+fi >&2
+
+if ([[ $tgt = "ja"]] || [[ $tgt = "zh"]]) && ! [[ -f $KYTEA_TOKENIZER ]]; then
     pushd $KYTEA_ROOT
     export CC
     ./configure --prefix=$(pwd)
@@ -108,23 +124,39 @@ if ! [[ -f $KYTEA_TOKENIZER ]]; then
     fi
 fi >&2
 
-if ! [[ -f $KYTEA_MODEL ]]; then
-    pushd $(dirname $KYTEA_MODEL)
-    curl -O http://www.phontron.com/kytea/download/model/$KYTEA_MODELNAME.gz
-    gzip -d $KYTEA_MODELNAME.gz
+if [[ $tgt = "ja" ]] && ! [[ -f $KYTEA_MODEL_JA ]]; then
+    pushd $(dirname $KYTEA_MODEL_JA)
+    curl -O http://www.phontron.com/kytea/download/model/$KYTEA_MODELNAME_JA.gz
+    gzip -d $KYTEA_MODELNAME_JA.gz
     popd
-    if ! [[ -f $KYTEA_MODEL ]]; then
+    if ! [[ -f $KYTEA_MODEL_JA ]]; then
         echo "kytea model not successfully downloaded, abort."
         exit -1
     fi
-fi >&2
+fi
+
+if [[ $tgt = "zh" ]] && ! [[ -f $KYTEA_MODEL_ZH ]]; then
+    pushd $(dirname $KYTEA_MODEL_ZH)
+    curl -O http://www.phontron.com/kytea/download/model/$KYTEA_MODELNAME_ZH.gz
+    gzip -d $KYTEA_MODELNAME_ZH.gz
+    popd
+    if ! [[ -f $KYTEA_MODEL_ZH ]]; then
+        echo "kytea model not successfully downloaded, abort."
+        exit -1
+    fi
+fi
 
 function extract() {
     mkdir -p $prep $orig
-    cp $TEST_DATA $orig/$ref.txt
+    cp $TEST_DATA $orig/$ref_filename
 
-    perl -ne 'chomp; @a=split/ \|\|\| /; print $a[2], "\n";' < $orig/$ref.txt > $orig/$ref.ja
-    perl -ne 'chomp; @a=split/ \|\|\| /; print $a[3], "\n";' < $orig/$ref.txt > $orig/$ref.en
+    if [[ $CORPORA = "ASPEC-JE" ]]; then
+        perl -ne 'chomp; @a=split/ \|\|\| /; print $a[2], "\n";' < $orig/$ref_filename > $orig/$test_set.ja
+        perl -ne 'chomp; @a=split/ \|\|\| /; print $a[3], "\n";' < $orig/$ref_filename > $orig/$test_set.en
+    elif [[ $CORPORA = "ASPEC-JC" ]]; then
+        perl -ne 'chomp; @a=split/ \|\|\| /; print $a[1], "\n";' < $orig/$ref_filename > $orig/$test_set.ja
+        perl -ne 'chomp; @a=split/ \|\|\| /; print $a[2], "\n";' < $orig/$ref_filename > $orig/$test_set.zh
+    fi
 }
 
 function tokenize_ja() {
@@ -132,7 +164,7 @@ function tokenize_ja() {
         perl -C -pe 'use utf8; s/(.)［[０-９．]+］$/${1}/;' | \
         sh $WAT_SCRIPTS/remove-space.sh | \
         perl -C $WAT_SCRIPTS/h2z-utf8-without-space.pl | \
-        $KYTEA_TOKENIZER -model $KYTEA_MODEL -out tok | \
+        $KYTEA_TOKENIZER -model $KYTEA_MODEL_JA -out tok | \
         perl -C -pe 's/^ +//; s/ +$//; s/ +/ /g;' | \
         perl -C -pe 'use utf8; while(s/([０-９]) ([０-９])/$1$2/g){} s/([０-９]) (．) ([０-９])/$1$2$3/g; while(s/([Ａ-Ｚ]) ([Ａ-Ｚａ-ｚ])/$1$2/g){} while(s/([ａ-ｚ]) ([ａ-ｚ])/$1$2/g){}'
 }
@@ -142,17 +174,42 @@ function tokenize_en() {
         perl -C $MOSES_TOKENIZER -l en -threads 8
 }
 
+function tokenize_zh() {
+    cat - | \
+        sh $WAT_SCRIPTS/remove-space.sh | \
+        perl -C $WAT_SCRIPTS/h2z-utf8-without-space.pl | \
+        $KYTEA_TOKENIZER -model $KYTEA_MODEL_ZH -out tok | \
+        perl -C -pe 's/^ +//; s/ +$//; s/ +/ /g;'
+}
+
 function tokenize() {
     tokenize_$1 2>/dev/null
 }
 
-if ! [[ -f $prep/$ref.$tgt ]]; then
+function eval_bleu() {
+    $MOSES_BLEU $prep/$test_set.$tgt 2>/dev/null
+}
+
+function eval_ribes() {
+    python3 $RIBES_SCRIPT -c -r $prep/$test_set.$tgt 2>/dev/null
+}
+
+function rmtemp() {
+    [[ -d $TMPOUT ]] && rm -rf $TMPOUT
+}
+
+if ! [[ -f $prep/$test_set.$tgt ]]; then
     extract >/dev/null 2>&1
-    cat $orig/$ref.$tgt | \
+    cat $orig/$test_set.$tgt | \
         tokenize $tgt \
-                 >$prep/$ref.$tgt
+                 >$prep/$test_set.$tgt
 fi
 
-cat - |
-    tokenize $tgt | \
-    $MOSES_BLEU $prep/$ref.$tgt 2>/dev/null
+TMPOUT=$(mktemp -d)
+trap rmtemp EXIT
+trap "rmtemp; exit 1" INT PIPE TERM
+sysout=$TMPOUT/sysout.tmp
+
+cat - | tokenize $tgt > $sysout
+cat $sysout | eval_bleu
+cat $sysout | eval_ribes
