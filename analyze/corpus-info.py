7f7670c357996e9c900599d19efdc5c00ce9f325
Hiroyuki Deguchi <deguchi@ai.cs.ehime-u.ac.jp>
2021-02-07 03:26:21 +0900

    upgrade: analyze/corpus-info.py

--- analyze/corpus-info.py
+++ analyze/corpus-info.py
@@ -15,9 +15,10 @@ SM_TICK = "▏"
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('--input', '-i', type=str, default='-', metavar='FILE')
-    parser.add_argument('--width-histogram', '-w', type=int, default=50, metavar='D')
-    parser.add_argument('--raw-size', '-r', type=int, default=50, metavar='D')
+    parser.add_argument("--input", "-i", type=str, default="-", metavar="FILE")
+    parser.add_argument("--no-histogram", action="store_true")
+    parser.add_argument("--histogram-width", "-w", type=int, default=50, metavar="D")
+    parser.add_argument("--raw-size", "-r", type=int, default=50, metavar="D")
     args = parser.parse_args()
     return args
 
@@ -25,55 +26,73 @@ def parse_args():
 def main(args):
     print(args, file=sys.stderr, flush=True)
 
-    num_tokens = 0
-    max_tokens = 0
-    max_tokens_id = 0
-    min_tokens = 1e9
-    min_tokens_id = 0
-    num_sentences = 0
+    ntokens = 0
+    squared_ntokens = 0
+    vocabulary = defaultdict(int)
+    max_len = 0
+    max_len_ids = []
+    min_len = 1e9
+    min_len_ids = []
+    nsentences = 0
     histogram = defaultdict(int)
 
     with fileinput.input(files=[args.input]) as f:
-        for line in f:
+        for sent_id, line in enumerate(f, start=1):
             line = line.strip().split()
+
+            for token in line:
+                vocabulary[token] += 1
+
             n = len(line)
+            ntokens += n
+            squared_ntokens += n**2
+            if n > max_len:
+                max_len_ids = [sent_id]
+                max_len = n
+            elif n == max_len:
+                max_len_ids.append(sent_id)
+            if n < min_len:
+                min_len_ids = [sent_id]
+                min_len = n
+            elif n == min_len:
+                min_len_ids.append(sent_id)
+            histogram[n // args.histogram_width] += 1
+            nsentences += 1
 
-            num_tokens += n
-            num_sentences += 1
-            if n > max_tokens:
-                max_tokens = n
-                max_tokens_id = num_sentences
-            if n < min_tokens:
-                min_tokens = n
-                min_tokens_id = num_sentences
-            histogram[n // args.width_histogram] += 1
+    ntokens_mean = ntokens / nsentences
+    ntokens_var = (squared_ntokens - ntokens_mean ** 2) / nsentences
+    ntokens_sd = ntokens_var ** 0.5
+    vocab_size = len(vocabulary)
 
-    print('num_sentences:\t{}'.format(num_sentences))
-    print('num_tokens:\t{}'.format(num_tokens))
-    print('average_tokens:\t{}'.format(num_tokens / num_sentences))
-    print('max_tokens:\t{}\t(line: {})'.format(max_tokens, max_tokens_id))
-    print('min_tokens:\t{}\t(line: {})'.format(min_tokens, min_tokens_id))
-    print('------------')
+    print("# of sentences     :\t{}".format(nsentences))
+    print("# of tokens        :\t{}".format(ntokens))
+    print("# of tokens (mean) :\t{}".format(ntokens_mean))
+    print("# of tokens (SD)   :\t{}".format(ntokens_sd))
+    print("max length         :\t{} (L.{})".format(max_len, ", ".join(map(str, max_len_ids))))
+    print("min length         :\t{} (L.{})".format(min_len, ", ".join(map(str, min_len_ids))))
+    print("vocabulary size    :\t{}".format(vocab_size))
 
-    print('histogram: (width={})'.format(args.width_histogram))
-    max_p = max(histogram)
-    max_value = max(histogram.values())
-    order = len(str(max_p * args.width_histogram))
-    graph_format = "{{:{}d}}-{{:{}d}}: |{{}}{{}} {{}}".format(order, order)
-    scaling = args.raw_size / max_value
-    for w in range(max_p):
-        count = histogram[w] * scaling
-        ticks = TICK * round(count)
-        if count != round(count):
-            ticks += SM_TICK
-        print(graph_format.format(
-            w * args.width_histogram,
-            (w + 1) * args.width_histogram,
-            ticks, " " * (args.raw_size - len(ticks)),
-            histogram[w]
-        ))
+    if not args.no_histogram:
+        print("------------")
+        print("histogram: (width={})".format(args.histogram_width))
+        max_p = max(histogram)
+        max_value = max(histogram.values())
+        order = len(str(max_p * args.histogram_width))
+        graph_format = "{{:{}d}}-{{:{}d}}: |{{}}{{}} {{}}".format(order, order)
+        scaling = args.raw_size / max_value
+        for w in range(max_p):
+            count = histogram[w] * scaling
+            ticks = TICK * round(count)
+            if count != round(count):
+                ticks += SM_TICK
+            print(graph_format.format(
+                w * args.histogram_width,
+                (w + 1) * args.histogram_width,
+                ticks, " " * (args.raw_size - len(ticks)),
+                histogram[w]
+            ))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     main(args)
